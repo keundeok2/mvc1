729ab24ffd2cac3420f0e28505146b10d7839ed5
keundeok2 <keundeok2@gmail.com>
2021-10-20 21:24:40 +0900

    회원관리 서블릿

---
+++ servlet/src/test/java/hello/servlet/domain/MemberRepositoryTest.java
@@ -0,0 +1,53 @@
+package hello.servlet.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberRepositoryTest {
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    /**
+     * 테스트 하나가 끝날 때 마다 실행
+     */
+    @AfterEach
+    void afterEach() {
+        memberRepository.clearStore();
+    }
+
+
+    @Test
+    void save() {
+        // given
+        Member member  = new Member("hello", 20);
+        // when
+        Member savedMember = memberRepository.save(member);
+
+        // then
+        Member findMember = memberRepository.findById(savedMember.getId());
+        assertThat(savedMember).isEqualTo(findMember);
+
+    }
+
+    @Test
+    void findAll() {
+        Member member1 = new Member("member1", 30);
+        Member member2 = new Member("member2", 20);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        List<Member> result = memberRepository.findAll();
+
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).contains(member1, member2);
+
+    }
+
+}
